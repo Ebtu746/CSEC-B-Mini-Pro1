63376eaa3f37f063c9269a6e68c75a8d2935d11e
Ebtu746 <marifamuhammed746@gmail.com>
2025-12-19 19:59:10 +0300

    Modifying the style

--- mini-pro3-Toggle_text/script.js
+++ mini-pro3-Toggle_text/script.js
@@ -6,9 +6,11 @@ let isOn = false;
 button.addEventListener("click", function () {
     if (isOn === false) {
         heading.textContent = "ON";
+        heading.style.color = "green";
         isOn = true;
     } else {
         heading.textContent = "OFF";
+        heading.style.color = "red";
         isOn = false;
     }
 });
